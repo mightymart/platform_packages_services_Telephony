9166ed8ac94cf66ffc0cf189bda7a20c74d009bb
Daniel Micay <danielmicay@gmail.com>
2017-07-14 22:14:40 -0400

    add LTE only option

--- src/com/android/phone/MobileNetworkSettings.java
+++ src/com/android/phone/MobileNetworkSettings.java
@@ -1172,6 +1172,7 @@ public class MobileNetworkSettings extends Activity  {
                         case Phone.NT_MODE_LTE_TDSCDMA_GSM_WCDMA:
                         case Phone.NT_MODE_TDSCDMA_CDMA_EVDO_GSM_WCDMA:
                         case Phone.NT_MODE_LTE_TDSCDMA_CDMA_EVDO_GSM_WCDMA:
+                        case Phone.NT_MODE_LTE_ONLY:
                             // This is one of the modes we recognize
                             modemNetworkMode = buttonNetworkMode;
                             break;
@@ -1349,9 +1350,12 @@ public class MobileNetworkSettings extends Activity  {
                             R.string.preferred_network_mode_evdo_only_summary);
                     break;
                 case Phone.NT_MODE_LTE_TDSCDMA:
-                case Phone.NT_MODE_LTE_ONLY:
                     mButtonPreferredNetworkMode.setSummary(
                             R.string.preferred_network_mode_lte_summary);
+                    break;
+                case Phone.NT_MODE_LTE_ONLY:
+                    mButtonPreferredNetworkMode.setSummary(
+                            R.string.preferred_network_mode_lte_only_summary);
                     break;
                 case Phone.NT_MODE_LTE_TDSCDMA_GSM:
                 case Phone.NT_MODE_LTE_TDSCDMA_GSM_WCDMA:
@@ -1437,6 +1441,11 @@ public class MobileNetworkSettings extends Activity  {
                         break;
                     }
                 case Phone.NT_MODE_LTE_ONLY:
+                    mButtonEnabledNetworks.setValue(
+                            Integer.toString(Phone.NT_MODE_LTE_ONLY));
+                    mButtonEnabledNetworks.setSummary((mShow4GForLTE == true)
+                            ? R.string.network_4G_only : R.string.network_lte_only);
+                    break;
                 case Phone.NT_MODE_LTE_WCDMA:
                     if (!mIsGlobalCdma) {
                         mButtonEnabledNetworks.setValue(
